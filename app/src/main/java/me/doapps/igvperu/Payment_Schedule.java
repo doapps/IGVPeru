fe27b9a3282e6f4805c906932c757d650cab3657
bryam <brayansoto4@hotmail.com>
2015-01-13 11:25:20 -0500

    create activity

---
+++ app/src/main/java/me/doapps/igvperu/Payment_Schedule.java
@@ -0,0 +1,12 @@
+package me.doapps.igvperu;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Bryam on 13/01/2015.
+ */
+public class Payment_Schedule extends ActionBarActivity{
+
+
+
+}
