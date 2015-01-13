3aaf592ff064d1195182103d57041af3a760c5f6
William <sulca.03.1995@gmail.com>
2015-01-13 11:29:11 -0500

    add layout in Payment schedule

--- app/src/main/java/me/doapps/igvperu/Payment_Schedule.java
+++ app/src/main/java/me/doapps/igvperu/Payment_Schedule.java
@@ -3,13 +3,11 @@ package me.doapps.igvperu;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-/**
- * Created by Bryam on 13/01/2015.
- */
 public class Payment_Schedule extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cronograma__pago);
     }
 }
