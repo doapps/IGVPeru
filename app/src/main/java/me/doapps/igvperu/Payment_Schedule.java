47a6748f263a453702e5ae701e7eaf2a7b730528
William <sulca.03.1995@gmail.com>
2015-01-13 11:31:58 -0500

    add method Hola  in Payment schedule

--- app/src/main/java/me/doapps/igvperu/Payment_Schedule.java
+++ app/src/main/java/me/doapps/igvperu/Payment_Schedule.java
@@ -2,6 +2,7 @@ package me.doapps.igvperu;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
 
 public class Payment_Schedule extends ActionBarActivity{
 
@@ -9,5 +10,10 @@ public class Payment_Schedule extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cronograma__pago);
+        Mensaje();
+    }
+
+    public void Mensaje(){
+        Toast.makeText(this,"HOLA",Toast.LENGTH_SHORT).show();
     }
 }
